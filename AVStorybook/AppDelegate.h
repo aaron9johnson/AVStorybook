dfc00a9230a86564cb635fed532b5954c5fbdd27
Aaron Johnson <aaron9johnson@gmail.com>
2017-10-20 11:37:19 -0700

    Initial Commit

---
+++ AVStorybook/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AVStorybook
+//
+//  Created by Aaron Johnson on 2017-10-20.
+//  Copyright © 2017 Aaron Johnson. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
